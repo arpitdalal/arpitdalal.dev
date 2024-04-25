3ee9d7c2e726dbb72b480c2c1ab43c8eccfda87f
Arpit Dalal <arpitdalalm@gmail.com>
2024-04-25 12:12:54 -0400

    refactor(styles): refactor logo svg to use tw

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -24,25 +24,22 @@ export function LogoSpinner({ className }: { className?: string }) {
 	return (
 		<div
 			className={cn(
-				'duration-default group-hover:duration-hover group-focus:duration-hover group-active:duration-active absolute cursor-pointer motion-safe:animate-spin',
+				'absolute cursor-pointer duration-default group-hover:duration-hover group-focus:duration-hover group-active:duration-active motion-safe:animate-spin',
 				className,
 			)}
 		>
 			<svg className="size-16" viewBox="0 0 56 56">
 				<path
 					d="M29.465,0.038373A28,28,0,0,1,52.948,40.712L51.166,39.804A26,26,0,0,0,29.361,2.0356Z"
-					className="text-current"
-					fill="currentColor"
+					className="fill-current"
 				></path>
 				<path
 					d="M51.483,43.250A28,28,0,0,1,4.5172,43.250L6.1946,42.161A26,26,0,0,0,49.805,42.161Z"
-					className="text-current"
-					fill="currentColor"
+					className="fill-current"
 				></path>
 				<path
 					d="M3.0518,40.712A28,28,0,0,1,26.535,0.038373L26.639,2.0356A26,26,0,0,0,4.8338,39.804Z"
-					className="text-current"
-					fill="currentColor"
+					className="fill-current"
 				></path>
 			</svg>
 		</div>
