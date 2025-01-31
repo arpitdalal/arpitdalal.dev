7cf76ae37f338a237a71c810742982661bb1a7cd
Arpit Dalal <arpitdalalm@gmail.com>
2025-01-30 21:42:27 -0500

    refactor(frontend): extract card and section components for reusability

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,133 +1,19 @@
-import { useWindowSize } from "@reactuses/core";
-import { motion, transform, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
-import ExternalLink from "#app/components/external-link";
-import { HighlightUnderline } from "#app/components/highlight";
-import { Badge } from "#app/components/ui/badge";
-import { Icon } from "#app/components/ui/icon";
 import { type Project } from "#app/routes/_marketing+/__data";
-import { useHints } from "#app/utils/client-hints";
-import { cn } from "#app/utils/misc";
-import {
-  HEADING_STYLES,
-  HEADING_STYLES_NO_JS_OR_MOTION_SAFE,
-} from "./animated-heading-styles";
-import { LineGlow } from "./line-glow";
+import { Card } from "./card";
+import { Section } from "./section";
 
-export default function Projects({
+export function Projects({
   projects,
   jsEnabled,
 }: {
   projects: Project[];
   jsEnabled: boolean;
 }) {
-  const sectionRef = useRef<HTMLElement>(null);
-  const { width } = useWindowSize();
-  const isXSScreen = width < 420;
-  const { scrollYProgress: scrollYProgressOfSection } = useScroll({
-    target: sectionRef,
-    offset: ["start start", "80px start"],
-  });
-  const { reducedMotion } = useHints();
-  const isReducedMotion = reducedMotion === "reduce";
-
-  const h2Left = useTransform(() => {
-    if (isReducedMotion || !jsEnabled) return HEADING_STYLES.LEFT_START;
-
-    return transform(
-      scrollYProgressOfSection.get(),
-      [0, 1],
-      [
-        HEADING_STYLES.LEFT_START,
-        isXSScreen
-          ? HEADING_STYLES.LEFT_END_SMALL_SCREEN
-          : HEADING_STYLES.LEFT_END,
-      ],
-    );
-  });
-
   return (
-    <section ref={sectionRef} id="projects">
-      <LineGlow />
-      <div className="container pb-12">
-        <motion.h2
-          style={{
-            paddingLeft: h2Left,
-          }}
-          className={cn(
-            "sticky top-12 z-40 max-w-fit xs:top-14 sm:top-12",
-            (!jsEnabled || isReducedMotion) &&
-              HEADING_STYLES_NO_JS_OR_MOTION_SAFE,
-          )}
-        >
-          <HighlightUnderline>Projects</HighlightUnderline>
-        </motion.h2>
-        <ol className="group/ol relative mt-8">
-          {projects.map((props) => (
-            <ProjectCard key={props.title} {...props} />
-          ))}
-        </ol>
-      </div>
-    </section>
-  );
-}
-
-function ProjectCard({
-  title,
-  link,
-  imageUrl,
-  imageAlt,
-  description,
-  tags,
-}: Project) {
-  return (
-    <li>
-      <article className="group relative grid py-8 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/ol:opacity-50">
-        <div
-          className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block motion-safe:lg:group-hover:bg-accent/60 motion-safe:lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] motion-safe:lg:group-hover:drop-shadow-lg"
-          aria-hidden
-        />
-        <div className="z-10 mb-2 mt-1 font-semibold uppercase tracking-wide text-foreground/70 sm:col-span-2 sm:pr-3 sm:text-right">
-          <img
-            src={imageUrl}
-            alt={imageAlt}
-            className="w-full drop-shadow-2xl"
-          />
-        </div>
-        <div className="z-10 max-sm:mt-4 sm:col-span-6">
-          <h3 className="font-medium">
-            <ExternalLink
-              href={link}
-              className="group/link inline-flex items-baseline font-medium leading-tight hover:text-primary focus-visible:text-primary"
-              aria-label={`${title} (opens in a new tab)`}
-            >
-              {/* This is for making the link clickable on the whole card */}
-              <span
-                className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"
-                aria-hidden
-              ></span>
-              <span>
-                {title}
-                <span className="inline-block">
-                  <Icon
-                    name="arrow-up-right-outline"
-                    className="ml-1 ease-in-out motion-safe:translate-y-px motion-safe:transition-transform motion-safe:group-hover/link:-translate-y-1 motion-safe:group-hover/link:translate-x-1 motion-safe:group-focus-visible/link:-translate-y-1 motion-safe:group-focus-visible/link:translate-x-1"
-                    aria-hidden
-                  />
-                </span>
-              </span>
-            </ExternalLink>
-          </h3>
-          <p className="mt-2 leading-normal">{description}</p>
-          {tags && tags.length > 0 ? (
-            <ul className="mt-4 flex flex-wrap gap-2" aria-label="Tags">
-              {tags.map((tag) => (
-                <Badge key={tag}>{tag}</Badge>
-              ))}
-            </ul>
-          ) : null}
-        </div>
-      </article>
-    </li>
+    <Section id="projects" jsEnabled={jsEnabled} sectionTitle="Projects">
+      {projects.map((props) => (
+        <Card key={props.title} {...props} />
+      ))}
+    </Section>
   );
 }
---
+++ app/components/section.tsx
@@ -0,0 +1,69 @@
+import { useWindowSize } from "@reactuses/core";
+import { motion, transform, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { HighlightUnderline } from "#app/components/highlight";
+import { useHints } from "#app/utils/client-hints";
+import { cn } from "#app/utils/misc";
+import {
+  HEADING_STYLES,
+  HEADING_STYLES_NO_JS_OR_MOTION_SAFE,
+} from "./animated-heading-styles";
+import { LineGlow } from "./line-glow";
+
+type SectionProps = {
+  jsEnabled: boolean;
+  sectionTitle: string;
+} & React.HTMLAttributes<HTMLElement>;
+
+export function Section({
+  jsEnabled,
+  sectionTitle,
+  children,
+  ...props
+}: SectionProps) {
+  const sectionRef = useRef<HTMLElement>(null);
+  const { width } = useWindowSize();
+  const isXSScreen = width < 420;
+  const { scrollYProgress: scrollYProgressOfSection } = useScroll({
+    target: sectionRef,
+    offset: ["start start", "80px start"],
+  });
+  const { reducedMotion } = useHints();
+  const isReducedMotion = reducedMotion === "reduce";
+
+  const h2Left = useTransform(() => {
+    if (isReducedMotion || !jsEnabled) return HEADING_STYLES.LEFT_START;
+
+    return transform(
+      scrollYProgressOfSection.get(),
+      [0, 1],
+      [
+        HEADING_STYLES.LEFT_START,
+        isXSScreen
+          ? HEADING_STYLES.LEFT_END_SMALL_SCREEN
+          : HEADING_STYLES.LEFT_END,
+      ],
+    );
+  });
+
+  return (
+    <section ref={sectionRef} {...props}>
+      <LineGlow />
+      <div className="container pb-12">
+        <motion.h2
+          style={{
+            paddingLeft: h2Left,
+          }}
+          className={cn(
+            "sticky top-12 z-40 max-w-fit xs:top-14 sm:top-12",
+            (!jsEnabled || isReducedMotion) &&
+              HEADING_STYLES_NO_JS_OR_MOTION_SAFE,
+          )}
+        >
+          <HighlightUnderline>{sectionTitle}</HighlightUnderline>
+        </motion.h2>
+        <ol className="group/ol relative mt-8">{children}</ol>
+      </div>
+    </section>
+  );
+}
