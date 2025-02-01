89e9080dbe7887e349b938a360971d32f6e12c34
Arpit Dalal <arpitdalalm@gmail.com>
2025-02-01 11:37:24 -0500

    refactor(404): extract NotFound component for reusability

--- app/components/error-boundary.tsx
+++ app/components/error-boundary.tsx
@@ -1,12 +1,18 @@
+import { useEventListener } from "@reactuses/core";
+import { type LinksFunction } from "@remix-run/node";
 import {
   type ErrorResponse,
   isRouteErrorResponse,
   useParams,
   useRouteError,
+  Link,
 } from "@remix-run/react";
 import { captureRemixErrorBoundaryError } from "@sentry/remix";
 import { type ReactElement } from "react";
+import DinoGame from "react-chrome-dino-ts";
+import reactChromeDinoCss from "react-chrome-dino-ts/index.css?url";
 import { getErrorMessage } from "#app/utils/misc";
+import { Icon } from "./ui/icon";
 
 type StatusHandler = (info: {
   error: ErrorResponse;
@@ -42,6 +48,39 @@ export function GeneralErrorBoundary({
             params,
           })
         : unexpectedErrorHandler(error)}
+    </div>
+  );
+}
+
+export const dinoCssLinks: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: reactChromeDinoCss }];
+};
+
+export function NotFound() {
+  useEventListener("keydown", (event) => {
+    if (event.code === "Space" && window.innerWidth >= 1024) {
+      // lg breakpoint
+      event.preventDefault();
+    }
+  });
+
+  return (
+    <div className="container flex items-center justify-center p-20 text-h2">
+      <div className="flex flex-col gap-6 pt-10">
+        <div className="flex flex-col gap-3">
+          <h1 className="text-h1">Lost, but not forgotten</h1>
+        </div>
+        <Link to="/" className="text-body-md underline">
+          <Icon name="arrow-left-outline">Let's find your way back</Icon>
+        </Link>
+        <div className="hidden w-full text-body-md lg:block">
+          <h2>Feel free to play a game while you're here</h2>
+          <DinoGame hideInstructions />
+          <p className="mt-6 text-center text-base text-foreground/70">
+            Press space to start the game.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,44 +1,13 @@
-import { type LinksFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
-import DinoGame from "react-chrome-dino-ts";
-import reactChromeDinoCss from "react-chrome-dino-ts/index.css?url";
-import { GeneralErrorBoundary } from "#app/components/error-boundary";
-import { Icon } from "#app/components/ui/icon";
+import { NotFound, dinoCssLinks } from "#app/components/error-boundary";
 
-export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: reactChromeDinoCss }];
-};
+export const links = () => [...dinoCssLinks()];
 
 export async function loader() {
   throw new Response("Not found", { status: 404 });
 }
 
-export default function NotFound() {
-  return <ErrorBoundary />;
-}
+export default NotFound;
 
 export function ErrorBoundary() {
-  return (
-    <GeneralErrorBoundary
-      statusHandlers={{
-        404: () => (
-          <div className="flex flex-col gap-6 pt-10">
-            <div className="flex flex-col gap-3">
-              <h1 className="text-h1">Lost, but not forgotten</h1>
-            </div>
-            <Link to="/" className="text-body-md underline">
-              <Icon name="arrow-left-outline">Let's find your way back</Icon>
-            </Link>
-            <div className="hidden w-full text-body-md lg:block">
-              <h2>Feel free to play a game while you're here</h2>
-              <DinoGame hideInstructions />
-              <p className="mt-6 text-center text-base text-foreground/70">
-                Press space to start the game.
-              </p>
-            </div>
-          </div>
-        ),
-      }}
-    />
-  );
+  return <NotFound />;
 }
