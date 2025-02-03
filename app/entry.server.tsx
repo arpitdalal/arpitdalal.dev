58827f902182c1bec71b8e0037e9ed8176eaabde
Arpit Dalal <arpitdalalm@gmail.com>
2025-02-03 17:04:47 -0500

    refactor(server): remove server timing

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,108 +1,103 @@
-import { PassThrough } from "node:stream";
-import { createReadableStreamFromReadable } from "@react-router/node";
-import * as Sentry from "@sentry/node";
-import chalk from "chalk";
-import { isbot } from "isbot";
-import { renderToPipeableStream } from "react-dom/server";
+import { PassThrough } from 'node:stream'
+import { createReadableStreamFromReadable } from '@react-router/node'
+import * as Sentry from '@sentry/node'
+import chalk from 'chalk'
+import { isbot } from 'isbot'
+import { renderToPipeableStream } from 'react-dom/server'
 import {
-  ServerRouter,
-  type LoaderFunctionArgs,
-  type ActionFunctionArgs,
-  type HandleDocumentRequestFunction,
-} from "react-router";
-import { getEnv, init } from "./utils/env.server";
-import { NonceProvider } from "./utils/nonce-provider";
-import { makeTimings } from "./utils/timing.server";
+	ServerRouter,
+	type LoaderFunctionArgs,
+	type ActionFunctionArgs,
+	type HandleDocumentRequestFunction,
+} from 'react-router'
+import { getEnv, init } from './utils/env.server'
+import { NonceProvider } from './utils/nonce-provider'
 
-export const streamTimeout = 5000;
+export const streamTimeout = 5000
 
-init();
-global.ENV = getEnv();
+init()
+global.ENV = getEnv()
 
-type DocRequestArgs = Parameters<HandleDocumentRequestFunction>;
+type DocRequestArgs = Parameters<HandleDocumentRequestFunction>
 
 export default async function handleRequest(...args: DocRequestArgs) {
-  const [
-    request,
-    responseStatusCode,
-    responseHeaders,
-    reactRouterContext,
-    loadContext,
-  ] = args;
-  responseHeaders.set("fly-region", process.env.FLY_REGION ?? "unknown");
-  responseHeaders.set("fly-app", process.env.FLY_APP_NAME ?? "unknown");
+	const [
+		request,
+		responseStatusCode,
+		responseHeaders,
+		reactRouterContext,
+		loadContext,
+	] = args
+	responseHeaders.set('fly-region', process.env.FLY_REGION ?? 'unknown')
+	responseHeaders.set('fly-app', process.env.FLY_APP_NAME ?? 'unknown')
 
-  if (process.env.NODE_ENV === "production" && process.env.SENTRY_DSN) {
-    responseHeaders.append("Document-Policy", "js-profiling");
-  }
+	if (process.env.NODE_ENV === 'production' && process.env.SENTRY_DSN) {
+		responseHeaders.append('Document-Policy', 'js-profiling')
+	}
 
-  const callbackName = isbot(request.headers.get("user-agent"))
-    ? "onAllReady"
-    : "onShellReady";
+	const callbackName = isbot(request.headers.get('user-agent'))
+		? 'onAllReady'
+		: 'onShellReady'
 
-  const nonce = loadContext.cspNonce?.toString() ?? "";
-  return new Promise(async (resolve, reject) => {
-    let didError = false;
-    // NOTE: this timing will only include things that are rendered in the shell
-    // and will not include suspended components and deferred loaders
-    const timings = makeTimings("render", "renderToPipeableStream");
+	const nonce = loadContext.cspNonce?.toString() ?? ''
+	return new Promise(async (resolve, reject) => {
+		let didError = false
 
-    const { pipe, abort } = renderToPipeableStream(
-      <NonceProvider value={nonce}>
-        <ServerRouter
-          context={reactRouterContext}
-          url={request.url}
-          nonce={nonce}
-        />
-      </NonceProvider>,
-      {
-        [callbackName]: () => {
-          const body = new PassThrough();
-          responseHeaders.set("Content-Type", "text/html");
-          responseHeaders.append("Server-Timing", timings.toString());
-          resolve(
-            new Response(createReadableStreamFromReadable(body), {
-              headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode,
-            }),
-          );
-          pipe(body);
-        },
-        onShellError: (err: unknown) => {
-          reject(err);
-        },
-        onError: () => {
-          didError = true;
-        },
-        nonce,
-      },
-    );
+		const { pipe, abort } = renderToPipeableStream(
+			<NonceProvider value={nonce}>
+				<ServerRouter
+					context={reactRouterContext}
+					url={request.url}
+					nonce={nonce}
+				/>
+			</NonceProvider>,
+			{
+				[callbackName]: () => {
+					const body = new PassThrough()
+					responseHeaders.set('Content-Type', 'text/html')
+					resolve(
+						new Response(createReadableStreamFromReadable(body), {
+							headers: responseHeaders,
+							status: didError ? 500 : responseStatusCode,
+						}),
+					)
+					pipe(body)
+				},
+				onShellError: (err: unknown) => {
+					reject(err)
+				},
+				onError: () => {
+					didError = true
+				},
+				nonce,
+			},
+		)
 
-    setTimeout(abort, streamTimeout + 5000);
-  });
+		setTimeout(abort, streamTimeout + 5000)
+	})
 }
 
 export async function handleDataRequest(response: Response) {
-  response.headers.set("fly-region", process.env.FLY_REGION ?? "unknown");
-  response.headers.set("fly-app", process.env.FLY_APP_NAME ?? "unknown");
+	response.headers.set('fly-region', process.env.FLY_REGION ?? 'unknown')
+	response.headers.set('fly-app', process.env.FLY_APP_NAME ?? 'unknown')
 
-  return response;
+	return response
 }
 
 export function handleError(
-  error: unknown,
-  { request }: LoaderFunctionArgs | ActionFunctionArgs,
+	error: unknown,
+	{ request }: LoaderFunctionArgs | ActionFunctionArgs,
 ): void {
-  // Skip capturing if the request is aborted as Remix docs suggest
-  // Ref: https://remix.run/docs/en/main/file-conventions/entry.server#handleerror
-  if (request.signal.aborted) {
-    return;
-  }
-  if (error instanceof Error) {
-    console.error(chalk.red(error.stack));
-    void Sentry.captureException(error);
-  } else {
-    console.error(error);
-    void Sentry.captureException(error);
-  }
+	// Skip capturing if the request is aborted as Remix docs suggest
+	// Ref: https://remix.run/docs/en/main/file-conventions/entry.server#handleerror
+	if (request.signal.aborted) {
+		return
+	}
+	if (error instanceof Error) {
+		console.error(chalk.red(error.stack))
+		void Sentry.captureException(error)
+	} else {
+		console.error(error)
+		void Sentry.captureException(error)
+	}
 }
--- app/root.tsx
+++ app/root.tsx
@@ -1,253 +1,246 @@
 import {
-  Link,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-} from "react-router";
-import { ClientOnly } from "remix-utils/client-only";
-import { HoneypotProvider } from "remix-utils/honeypot/react";
-import appleTouchIconAssetUrl from "#app/assets/favicons/apple-touch-icon.png";
-import faviconAssetUrl from "#app/assets/favicons/favicon.png";
-import { GeneralErrorBoundary } from "#app/components/error-boundary";
-import ExternalLink from "#app/components/external-link";
-import { Header } from "#app/components/header";
-import { Logo, LogoCircle, LogoImage, LogoSpinner } from "#app/components/logo";
-import { Newsletter } from "#app/components/newsletter";
-import { EpicProgress } from "#app/components/progress-bar";
-import { href as iconsHref } from "#app/components/ui/icon";
-import tailwindStyleSheetUrl from "#app/styles/tailwind.css?url";
-import { ClientHintCheck, getHints, useHints } from "#app/utils/client-hints";
-import { getEnv } from "#app/utils/env.server";
-import { honeypot } from "#app/utils/honeypot.server";
-import { capitalize, getDomainUrl, getUrl } from "#app/utils/misc";
-import { useNonce } from "#app/utils/nonce-provider";
-import { getSocialMetas } from "#app/utils/seo";
-import { type Theme } from "#types/index";
-import { type Route } from "./+types/root.ts";
+	Link,
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLoaderData,
+} from 'react-router'
+import { ClientOnly } from 'remix-utils/client-only'
+import { HoneypotProvider } from 'remix-utils/honeypot/react'
+import appleTouchIconAssetUrl from '#app/assets/favicons/apple-touch-icon.png'
+import faviconAssetUrl from '#app/assets/favicons/favicon.png'
+import { GeneralErrorBoundary } from '#app/components/error-boundary'
+import ExternalLink from '#app/components/external-link'
+import { Header } from '#app/components/header'
+import { Logo, LogoCircle, LogoImage, LogoSpinner } from '#app/components/logo'
+import { Newsletter } from '#app/components/newsletter'
+import { EpicProgress } from '#app/components/progress-bar'
+import { href as iconsHref } from '#app/components/ui/icon'
+import tailwindStyleSheetUrl from '#app/styles/tailwind.css?url'
+import { ClientHintCheck, getHints, useHints } from '#app/utils/client-hints'
+import { getEnv } from '#app/utils/env.server'
+import { honeypot } from '#app/utils/honeypot.server'
+import { capitalize, getDomainUrl, getUrl } from '#app/utils/misc'
+import { useNonce } from '#app/utils/nonce-provider'
+import { getSocialMetas } from '#app/utils/seo'
+import { type Theme } from '#types/index'
+import { type Route } from './+types/root.ts'
 
 export const links: Route.LinksFunction = () => {
-  return [
-    { rel: "preload", href: iconsHref, as: "image" },
-    // { rel: "preload", href: fontStyleStyleSheetUrl, as: "style" },
-    { rel: "preload", href: tailwindStyleSheetUrl, as: "style" },
-    {
-      rel: "icon",
-      href: "/favicon.ico",
-      sizes: "48x48",
-    },
-    { rel: "icon", type: "image/png", href: faviconAssetUrl },
-    { rel: "apple-touch-icon", href: appleTouchIconAssetUrl },
-    {
-      rel: "manifest",
-      href: "/site.webmanifest",
-      crossOrigin: "use-credentials",
-    } as const, // need this to make TS happy
-    // { rel: "stylesheet", href: fontStyleStyleSheetUrl },
-    { rel: "stylesheet", href: tailwindStyleSheetUrl },
-  ].filter(Boolean);
-};
-
-export const meta: Route.MetaFunction = ({ data }) => {
-  const requestInfo = data?.requestInfo;
-  return [
-    { viewport: "width=device-width,initial-scale=1,viewport-fit=cover" },
-    {
-      "theme-color": requestInfo?.hints.theme === "dark" ? "#1F2028" : "#FFF",
-    },
-    ...getSocialMetas({
-      url: getUrl(requestInfo),
-    }),
-  ];
-};
-
-export async function loader({ request }: Route.LoaderArgs) {
-  const honeyProps = await honeypot.getInputProps();
-
-  return {
-    requestInfo: {
-      hints: getHints(request),
-      origin: getDomainUrl(request),
-      path: new URL(request.url).pathname,
-    },
-    ENV: getEnv(),
-    honeyProps,
-  };
+	return [
+		{ rel: 'preload', href: iconsHref, as: 'image' },
+		// { rel: "preload", href: fontStyleStyleSheetUrl, as: "style" },
+		{ rel: 'preload', href: tailwindStyleSheetUrl, as: 'style' },
+		{
+			rel: 'icon',
+			href: '/favicon.ico',
+			sizes: '48x48',
+		},
+		{ rel: 'icon', type: 'image/png', href: faviconAssetUrl },
+		{ rel: 'apple-touch-icon', href: appleTouchIconAssetUrl },
+		{
+			rel: 'manifest',
+			href: '/site.webmanifest',
+			crossOrigin: 'use-credentials',
+		} as const, // need this to make TS happy
+		// { rel: "stylesheet", href: fontStyleStyleSheetUrl },
+		{ rel: 'stylesheet', href: tailwindStyleSheetUrl },
+	].filter(Boolean)
 }
 
-export const headers: Route.HeadersFunction = ({ loaderHeaders }) => {
-  const headers = {
-    "Server-Timing": loaderHeaders.get("Server-Timing") ?? "",
-  };
-  return headers;
-};
+export const meta: Route.MetaFunction = ({ data }) => {
+	const requestInfo = data?.requestInfo
+	return [
+		{ viewport: 'width=device-width,initial-scale=1,viewport-fit=cover' },
+		{
+			'theme-color': requestInfo?.hints.theme === 'dark' ? '#1F2028' : '#FFF',
+		},
+		...getSocialMetas({
+			url: getUrl(requestInfo),
+		}),
+	]
+}
+
+export async function loader({ request }: Route.LoaderArgs) {
+	const honeyProps = await honeypot.getInputProps()
+
+	return {
+		requestInfo: {
+			hints: getHints(request),
+			origin: getDomainUrl(request),
+			path: new URL(request.url).pathname,
+		},
+		ENV: getEnv(),
+		honeyProps,
+	}
+}
 
 function Document({
-  children,
-  nonce,
-  theme,
-  env = {},
+	children,
+	nonce,
+	theme,
+	env = {},
 }: {
-  children: React.ReactNode;
-  nonce: string;
-  theme?: Theme;
-  env?: Record<string, string | undefined>;
+	children: React.ReactNode
+	nonce: string
+	theme?: Theme
+	env?: Record<string, string | undefined>
 }) {
-  const { theme: hintTheme } = useHints();
+	const { theme: hintTheme } = useHints()
 
-  return (
-    <html
-      lang="en"
-      className={`${theme || hintTheme || "light"} h-full overflow-x-hidden`}
-    >
-      <head>
-        <ClientHintCheck nonce={nonce} />
-        <Meta />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Links />
-      </head>
-      <body className="bg-background text-foreground">
-        {children}
-        <script
-          nonce={nonce}
-          dangerouslySetInnerHTML={{
-            __html: `window.ENV = ${JSON.stringify(env)}`,
-          }}
-        />
-        <ScrollRestoration nonce={nonce} />
-        <Scripts nonce={nonce} />
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className={`${theme || hintTheme || 'light'} h-full overflow-x-hidden`}
+		>
+			<head>
+				<ClientHintCheck nonce={nonce} />
+				<Meta />
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
+				<Links />
+			</head>
+			<body className="bg-background text-foreground">
+				{children}
+				<script
+					nonce={nonce}
+					dangerouslySetInnerHTML={{
+						__html: `window.ENV = ${JSON.stringify(env)}`,
+					}}
+				/>
+				<ScrollRestoration nonce={nonce} />
+				<Scripts nonce={nonce} />
+			</body>
+		</html>
+	)
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
-  // if there was an error running the loader, data could be missing
-  const data = useLoaderData<typeof loader | null>();
-  const nonce = useNonce();
+	// if there was an error running the loader, data could be missing
+	const data = useLoaderData<typeof loader | null>()
+	const nonce = useNonce()
 
-  return (
-    <Document
-      nonce={nonce}
-      theme={data?.requestInfo?.hints.theme}
-      env={data?.ENV}
-    >
-      {children}
-    </Document>
-  );
+	return (
+		<Document
+			nonce={nonce}
+			theme={data?.requestInfo?.hints.theme}
+			env={data?.ENV}
+		>
+			{children}
+		</Document>
+	)
 }
 
 function App() {
-  return (
-    <>
-      <div className="flex min-h-screen flex-col justify-between">
-        <ClientOnly fallback={<Header jsEnabled={false} />}>
-          {() => <Header jsEnabled />}
-        </ClientOnly>
-        <main id="main" className="flex-1 pb-20">
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-      <EpicProgress />
-    </>
-  );
+	return (
+		<>
+			<div className="flex min-h-screen flex-col justify-between">
+				<ClientOnly fallback={<Header jsEnabled={false} />}>
+					{() => <Header jsEnabled />}
+				</ClientOnly>
+				<main id="main" className="flex-1 pb-20">
+					<Outlet />
+				</main>
+				<Footer />
+			</div>
+			<EpicProgress />
+		</>
+	)
 }
 
 function AppWithProviders() {
-  const data = useLoaderData<typeof loader>();
+	const data = useLoaderData<typeof loader>()
 
-  return (
-    <HoneypotProvider {...data.honeyProps}>
-      <App />
-    </HoneypotProvider>
-  );
+	return (
+		<HoneypotProvider {...data.honeyProps}>
+			<App />
+		</HoneypotProvider>
+	)
 }
 
-export default AppWithProviders;
+export default AppWithProviders
 
 export const headerAndFooterCommonLinks = {
-  contact: "contact",
-};
+	contact: 'contact',
+}
 
 function Footer() {
-  return (
-    <footer className="border-t border-foreground/40">
-      <div className="container flex flex-wrap items-start justify-between gap-10 py-5">
-        <Link
-          to="/"
-          className="group z-10 flex items-center gap-4 outline-none ring-ring ring-offset-2 ring-offset-background transition-colors focus-within:ring-2 focus-visible:ring-2"
-        >
-          <Logo>
-            <LogoCircle />
-            <LogoSpinner />
-            <LogoImage />
-          </Logo>
-          <span
-            className="underlined text-h5 xs:text-h2"
-            data-content="Arpit Dalal"
-          >
-            Arpit Dalal
-          </span>
-        </Link>
-        <nav className="grow">
-          <p className="text-lg">
-            <strong>Pages</strong>
-          </p>
-          <ul className="mt-3 flex flex-col gap-1">
-            <li>
-              <Link
-                className="underlined text-foreground/70"
-                to="/"
-                data-content="Home"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <ExternalLink
-                href="https://blog.arpitdalal.dev"
-                applyBaseClassName={false}
-              >
-                Blog
-              </ExternalLink>
-            </li>
-            {Object.entries(headerAndFooterCommonLinks).map(([key, value]) => (
-              <li key={key}>
-                <Link
-                  to={value}
-                  className="underlined text-foreground/70"
-                  data-content={capitalize(key)}
-                >
-                  {capitalize(key)}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div>
-          <h2 className="text-lg font-bold xs:text-2xl">
-            Subscribe to my newsletter
-          </h2>
-          <p className="mt-2 text-sm text-foreground/70">
-            Get the latest updates from me directly to your inbox, no spam.
-          </p>
-          <Newsletter />
-        </div>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className="border-t border-foreground/40">
+			<div className="container flex flex-wrap items-start justify-between gap-10 py-5">
+				<Link
+					to="/"
+					className="group z-10 flex items-center gap-4 outline-none ring-ring ring-offset-2 ring-offset-background transition-colors focus-within:ring-2 focus-visible:ring-2"
+				>
+					<Logo>
+						<LogoCircle />
+						<LogoSpinner />
+						<LogoImage />
+					</Logo>
+					<span
+						className="underlined text-h5 xs:text-h2"
+						data-content="Arpit Dalal"
+					>
+						Arpit Dalal
+					</span>
+				</Link>
+				<nav className="grow">
+					<p className="text-lg">
+						<strong>Pages</strong>
+					</p>
+					<ul className="mt-3 flex flex-col gap-1">
+						<li>
+							<Link
+								className="underlined text-foreground/70"
+								to="/"
+								data-content="Home"
+							>
+								Home
+							</Link>
+						</li>
+						<li>
+							<ExternalLink
+								href="https://blog.arpitdalal.dev"
+								applyBaseClassName={false}
+							>
+								Blog
+							</ExternalLink>
+						</li>
+						{Object.entries(headerAndFooterCommonLinks).map(([key, value]) => (
+							<li key={key}>
+								<Link
+									to={value}
+									className="underlined text-foreground/70"
+									data-content={capitalize(key)}
+								>
+									{capitalize(key)}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</nav>
+				<div>
+					<h2 className="text-lg font-bold xs:text-2xl">
+						Subscribe to my newsletter
+					</h2>
+					<p className="mt-2 text-sm text-foreground/70">
+						Get the latest updates from me directly to your inbox, no spam.
+					</p>
+					<Newsletter />
+				</div>
+			</div>
+		</footer>
+	)
 }
 
 export function ErrorBoundary() {
-  const nonce = useNonce();
+	const nonce = useNonce()
 
-  return (
-    <Document nonce={nonce}>
-      <GeneralErrorBoundary />
-    </Document>
-  );
+	return (
+		<Document nonce={nonce}>
+			<GeneralErrorBoundary />
+		</Document>
+	)
 }
