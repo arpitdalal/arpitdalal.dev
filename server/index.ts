2371c4c1212dd2e17856120f210355e50128e2bb
Arpit Dalal <arpitdalalm@gmail.com>
2025-02-06 12:09:57 -0500

    fix(server): only apply rate limiting for contact and newsletter routes

--- server/index.ts
+++ server/index.ts
@@ -167,7 +167,7 @@ const strongRateLimit = rateLimit({
 
 const generalRateLimit = rateLimit(rateLimitDefault);
 app.use((req, res, next) => {
-  const strongPaths = ["/"];
+  const strongPaths = ["/contact", "/resources/newsletter"];
   if (req.method !== "GET" && req.method !== "HEAD") {
     if (strongPaths.some((p) => req.path.includes(p))) {
       return strongestRateLimit(req, res, next);
