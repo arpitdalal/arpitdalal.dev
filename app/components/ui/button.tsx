fe8d2d50ec536ba5f90352c70f1e70c6b3169b66
Arpit Dalal <arpitdalalm@gmail.com>
2025-02-20 12:32:08 -0500

    feat(ui): enhance button component ring color to match with its variants
    - Add ring variants for destructive and success button styles

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -4,16 +4,17 @@ import * as React from 'react'
 import { cn } from '#app/utils/misc'
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors outline-hidden focus-visible:ring-2 focus-within:ring-2 ring-ring ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+	'ring-offset-background ring-ring inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-2 outline-hidden transition-colors focus-within:ring-2 focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
 	{
 		variants: {
 			variant: {
 				default: 'bg-primary text-primary-foreground hover:bg-primary/80',
 				destructive:
-					'bg-destructive text-destructive-foreground hover:bg-destructive/80',
-				success: 'bg-success text-success-foreground hover:bg-success/80',
+					'bg-destructive text-destructive-foreground hover:bg-destructive/80 ring-foreground-destructive',
+				success:
+					'bg-success text-success-foreground hover:bg-success/80 ring-foreground-success',
 				outline:
-					'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+					'border-input bg-background hover:bg-accent hover:text-accent-foreground border',
 				secondary:
 					'bg-secondary text-secondary-foreground hover:bg-secondary/80',
 				ghost: 'hover:bg-accent hover:text-accent-foreground',
