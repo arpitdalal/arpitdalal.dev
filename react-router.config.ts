252d64e8145d8129d74f0383cdd9c242632c2576
Arpit Dalal <arpitdalalm@gmail.com>
2026-01-29 11:22:09 -0500

    chore: update deps

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,7 @@ import { type Config } from '@react-router/dev/config'
 export default {
 	future: {
 		unstable_optimizeDeps: true,
-		unstable_splitRouteModules: true,
-		unstable_viteEnvironmentApi: true,
+		v8_splitRouteModules: true,
+		v8_viteEnvironmentApi: true,
 	},
 } satisfies Config
