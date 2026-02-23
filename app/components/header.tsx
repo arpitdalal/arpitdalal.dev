1dc27fa1f1acd4ea6667b97744c33ce0eb2e85cb
Arpit Dalal <arpitdalalm@gmail.com>
2026-02-23 13:01:07 -0500

    fix: refactor back to pre liquid glass header because current impl of liquid glass wasn't working on Safari, and no other good alts that have good react bindings and work on all browsers

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,5 +1,4 @@
-import { LiquidGlass } from '@liquidglass/react'
-import { useMeasure, useMediaQuery, useWindowSize } from '@reactuses/core'
+import { useMeasure, useWindowSize } from '@reactuses/core'
 import {
 	AnimatePresence,
 	motion,
@@ -40,6 +39,10 @@ const DIV_STYLES = {
 	SHADOW_OPACITY_START: 0,
 	SHADOW_OPACITY_END: 0.1,
 }
+const NAV_STYLES = {
+	PADDING_START: '0 2rem',
+	PADDING_END: '0 1rem',
+}
 const TEXT_STYLES = {
 	OPACITY_START: 1,
 	OPACITY_END: 0,
@@ -65,7 +68,6 @@ const mobileButtonVariants = {
 export function Header({ jsEnabled }: { jsEnabled: boolean }) {
 	const [isMobileNavOpen, setIsMobileNavOpen] = useState(false)
 	const { width: windowWidth } = useWindowSize()
-	const isMobile = useMediaQuery('(max-width: 48rem)')
 	const { reducedMotion } = useHints()
 	const { scrollY } = useScroll()
 	const isReducedMotion = reducedMotion === 'reduce'
@@ -128,11 +130,17 @@ export function Header({ jsEnabled }: { jsEnabled: boolean }) {
 		)
 	})
 	const divBackdropBlur = useTransform(() => {
-		if (!isMobile) return undefined
-		if (!isMobileNavOpen) return 'none'
-		if (!isScrollable) return `blur(${DIV_STYLES.BACKDROP_BLUR_START})`
+		if (!isScrollable) {
+			if (isMobileNavOpen) {
+				return `blur(${DIV_STYLES.BACKDROP_BLUR_END})`
+			}
+			return `blur(${DIV_STYLES.BACKDROP_BLUR_START})`
+		}
 		if (!jsEnabled) return `blur(${DIV_STYLES.BACKDROP_BLUR_END})`
-		if (scrollY.get() === 0) return `blur(${DIV_STYLES.BACKDROP_BLUR_END})`
+
+		if (isMobileNavOpen && scrollY.get() === 0) {
+			return `blur(${DIV_STYLES.BACKDROP_BLUR_END})`
+		}
 
 		const transformedValue = transform(
 			scrollY.get(),
@@ -142,15 +150,15 @@ export function Header({ jsEnabled }: { jsEnabled: boolean }) {
 		return `blur(${transformedValue})`
 	})
 	const divBgColor = useTransform(() => {
-		if (!isMobile) return undefined
-		if (!isMobileNavOpen)
-			return `hsl(var(--accent) / ${DIV_STYLES.BG_OPACITY_START})`
 		if (!isScrollable) {
-			return `hsl(var(--accent) / ${DIV_STYLES.BG_OPACITY_END})`
+			if (isMobileNavOpen) {
+				return `hsl(var(--accent) / ${DIV_STYLES.BG_OPACITY_END})`
+			}
+			return `hsl(var(--accent) / ${DIV_STYLES.BG_OPACITY_START})`
 		}
 		if (!jsEnabled) return `hsl(var(--accent) / ${DIV_STYLES.BG_OPACITY_START})`
 
-		if (scrollY.get() === 0) {
+		if (isMobileNavOpen && scrollY.get() === 0) {
 			return `hsl(var(--accent) / ${DIV_STYLES.BG_OPACITY_END})`
 		}
 
@@ -171,6 +179,16 @@ export function Header({ jsEnabled }: { jsEnabled: boolean }) {
 			[DIV_STYLES.SHADOW_OPACITY_START, DIV_STYLES.SHADOW_OPACITY_END],
 		)
 		return `0 10px 15px -3px rgb(0 0 0 / ${transformedValue}), 0 4px 6px -4px rgb(0 0 0 / ${transformedValue})`
+	})
+	const navPadding = useTransform(() => {
+		if (!jsEnabled || isReducedMotion || !isScrollable)
+			return NAV_STYLES.PADDING_START
+
+		return transform(
+			scrollY.get(),
+			[0, SCROLL_THRESHOLD_PX],
+			[NAV_STYLES.PADDING_START, NAV_STYLES.PADDING_END],
+		)
 	})
 	const textOpacity = useTransform(() => {
 		if (!jsEnabled || isReducedMotion || !isScrollable)
@@ -209,7 +227,7 @@ export function Header({ jsEnabled }: { jsEnabled: boolean }) {
 			}}
 		>
 			<motion.div
-				className="border-foreground/40 mx-auto overflow-hidden border-solid"
+				className="border-foreground/40 mx-auto border-solid py-4"
 				style={{
 					borderRadius: divBorderRadius,
 					maxWidth: divMaxWidth,
@@ -219,164 +237,158 @@ export function Header({ jsEnabled }: { jsEnabled: boolean }) {
 					boxShadow: divShadowOpacity,
 				}}
 			>
-				<LiquidGlass
-					className={cn(
-						'w-full',
-						isMobile && isMobileNavOpen && '[&>div>span]:hidden [&>svg]:hidden',
-					)}
-					borderRadius={0}
-					blur={2}
+				<motion.nav
+					className="container"
+					style={{
+						padding: navPadding,
+					}}
 				>
-					<div className="w-full p-4">
-						<motion.nav className="container p-0">
-							<div className="flex items-center justify-between gap-4 md:gap-8">
-								<Link
-									className="bg-primary ring-ring ring-offset-background absolute -top-96 left-0 z-50 m-3 p-3 opacity-0 ring-offset-2 outline-hidden transition-all duration-200 ease-in-out focus-within:ring-2 focus:top-0 focus:opacity-100 focus-visible:ring-2"
-									to="#main"
-								>
-									Skip Navigation
-								</Link>
-								<Link
-									to={href('/')}
-									className="group ring-ring ring-offset-background z-10 flex items-center gap-4 overflow-clip ring-offset-2 outline-hidden transition-colors focus-within:ring-2 focus-visible:ring-2"
-								>
-									<Logo className="z-10">
-										<LogoCircle />
-										<LogoSpinner />
-										<LogoImage />
-									</Logo>
-									<motion.span
-										style={{
-											opacity: textOpacity,
-											x: textX,
-										}}
-										className="underlined text-h5 xs:text-h2"
-										ref={spanRef}
-										data-content="Arpit Dalal"
+					<div className="flex items-center justify-between gap-4 md:gap-8">
+						<Link
+							className="bg-primary ring-ring ring-offset-background absolute -top-96 left-0 z-50 m-3 p-3 opacity-0 ring-offset-2 outline-hidden transition-all duration-200 ease-in-out focus-within:ring-2 focus:top-0 focus:opacity-100 focus-visible:ring-2"
+							to="#main"
+						>
+							Skip Navigation
+						</Link>
+						<Link
+							to={href('/')}
+							className="group ring-ring ring-offset-background z-10 flex items-center gap-4 overflow-clip ring-offset-2 outline-hidden transition-colors focus-within:ring-2 focus-visible:ring-2"
+						>
+							<Logo className="z-10">
+								<LogoCircle />
+								<LogoSpinner />
+								<LogoImage />
+							</Logo>
+							<motion.span
+								style={{
+									opacity: textOpacity,
+									x: textX,
+								}}
+								className="underlined text-h5 xs:text-h2"
+								ref={spanRef}
+								data-content="Arpit Dalal"
+							>
+								Arpit Dalal
+							</motion.span>
+						</Link>
+						<ul className="hidden items-center gap-4 md:flex md:gap-8">
+							{headerAndFooterCommonExternalLinks.map((link) => (
+								<li key={link.title}>
+									<ExternalLink href={link.link} applyRingClassName={false}>
+										{link.title}
+									</ExternalLink>
+								</li>
+							))}
+							{headerAndFooterCommonInternalLinks.map((link) => (
+								<li key={link.title}>
+									<NavLink
+										to={link.link}
+										className="underlined text-foreground/70"
+										data-content={link.title}
 									>
-										Arpit Dalal
-									</motion.span>
-								</Link>
-								<ul className="hidden items-center gap-4 md:flex md:gap-8">
-									{headerAndFooterCommonExternalLinks.map((link) => (
-										<li key={link.title}>
-											<ExternalLink href={link.link} applyRingClassName={false}>
-												{link.title}
-											</ExternalLink>
-										</li>
-									))}
-									{headerAndFooterCommonInternalLinks.map((link) => (
-										<li key={link.title}>
-											<NavLink
-												to={link.link}
-												className="underlined text-foreground/70"
-												data-content={link.title}
-											>
-												{link.title}
-											</NavLink>
-										</li>
-									))}
-								</ul>
-								<div className="md:hidden">
-									<motion.button
-										initial="closed"
-										animate={isMobileNavOpen ? 'open' : 'closed'}
-										onClick={toggleMobileNav}
-										className="relative z-40 flex flex-col space-y-1"
-									>
-										<svg
-											width="32"
-											height="32"
-											viewBox="0 0 32 32"
-											fill="none"
-											xmlns="http://www.w3.org/2000/svg"
-										>
-											<motion.rect
-												variants={mobileButtonVariants.topLine}
-												transition={transition}
-												style={{
-													transformBox: 'view-box', // Needed to fix the mobile menu's x icon animation.
-												}}
-												x="6"
-												y="9"
-												width="20"
-												height="2"
-												rx="1"
-												fill="currentColor"
-											/>
-											<motion.rect
-												variants={mobileButtonVariants.centerLine}
-												transition={transition}
-												x="6"
-												y="15"
-												width="20"
-												height="2"
-												rx="1"
-												fill="currentColor"
-											/>
-											<motion.rect
-												variants={mobileButtonVariants.bottomLine}
-												transition={transition}
-												style={{
-													transformBox: 'view-box', // Needed to fix the mobile menu's x icon animation.
-												}}
-												x="6"
-												y="21"
-												width="20"
-												height="2"
-												rx="1"
-												fill="currentColor"
-											/>
-										</svg>
-										<span className="sr-only">
-											{isMobileNavOpen ? 'Close' : 'Open'} navbar
-										</span>
-									</motion.button>
-								</div>
-							</div>
-							<AnimatePresence>
-								{isMobileNavOpen && (
-									<motion.ul
-										className="divide-foreground/20 flex flex-col divide-y overflow-hidden md:hidden"
-										initial={{ height: 0 }}
-										animate={{ height: 'auto' }}
-										exit={{ height: 0 }}
+										{link.title}
+									</NavLink>
+								</li>
+							))}
+						</ul>
+						<div className="md:hidden">
+							<motion.button
+								initial="closed"
+								animate={isMobileNavOpen ? 'open' : 'closed'}
+								onClick={toggleMobileNav}
+								className="relative z-40 flex flex-col space-y-1"
+							>
+								<svg
+									width="32"
+									height="32"
+									viewBox="0 0 32 32"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<motion.rect
+										variants={mobileButtonVariants.topLine}
 										transition={transition}
-									>
-										{headerAndFooterCommonExternalLinks.map((link) => (
-											<li className="first:pt-4 last:pb-3" key={link.title}>
-												<ExternalLink
-													href={link.link}
-													className="text-foreground/70 hover:text-foreground block w-full py-4"
-													data-content={link.title}
-													applyUnderlineClassName={false}
-												>
-													{link.title}
-												</ExternalLink>
-											</li>
-										))}
-										{headerAndFooterCommonInternalLinks.map((link) => (
-											<li key={link.title} className="last:pb-3">
-												<NavLink
-													to={link.link}
-													className={({ isActive }) =>
-														cn(
-															`text-foreground/70 hover:text-foreground block w-full py-4`,
-															isActive && 'text-foreground',
-														)
-													}
-													data-content={link.title}
-												>
-													{link.title}
-												</NavLink>
-											</li>
-										))}
-									</motion.ul>
-								)}
-							</AnimatePresence>
-						</motion.nav>
+										style={{
+											transformBox: 'view-box', // Needed to fix the mobile menu's x icon animation.
+										}}
+										x="6"
+										y="9"
+										width="20"
+										height="2"
+										rx="1"
+										fill="currentColor"
+									/>
+									<motion.rect
+										variants={mobileButtonVariants.centerLine}
+										transition={transition}
+										x="6"
+										y="15"
+										width="20"
+										height="2"
+										rx="1"
+										fill="currentColor"
+									/>
+									<motion.rect
+										variants={mobileButtonVariants.bottomLine}
+										transition={transition}
+										style={{
+											transformBox: 'view-box', // Needed to fix the mobile menu's x icon animation.
+										}}
+										x="6"
+										y="21"
+										width="20"
+										height="2"
+										rx="1"
+										fill="currentColor"
+									/>
+								</svg>
+								<span className="sr-only">
+									{isMobileNavOpen ? 'Close' : 'Open'} navbar
+								</span>
+							</motion.button>
+						</div>
 					</div>
-				</LiquidGlass>
+					<AnimatePresence>
+						{isMobileNavOpen && (
+							<motion.ul
+								className="divide-foreground/20 flex flex-col divide-y overflow-hidden md:hidden"
+								initial={{ height: 0 }}
+								animate={{ height: 'auto' }}
+								exit={{ height: 0 }}
+								transition={transition}
+							>
+								{headerAndFooterCommonExternalLinks.map((link) => (
+									<li className="first:pt-4 last:pb-3" key={link.title}>
+										<ExternalLink
+											href={link.link}
+											className="text-foreground/70 hover:text-foreground block w-full py-4"
+											data-content={link.title}
+											applyUnderlineClassName={false}
+										>
+											{link.title}
+										</ExternalLink>
+									</li>
+								))}
+								{headerAndFooterCommonInternalLinks.map((link) => (
+									<li key={link.title} className="last:pb-3">
+										<NavLink
+											to={link.link}
+											className={({ isActive }) =>
+												cn(
+													`text-foreground/70 hover:text-foreground block w-full py-4`,
+													isActive && 'text-foreground',
+												)
+											}
+											data-content={link.title}
+										>
+											{link.title}
+										</NavLink>
+									</li>
+								))}
+							</motion.ul>
+						)}
+					</AnimatePresence>
+				</motion.nav>
 			</motion.div>
 		</motion.header>
 	)
